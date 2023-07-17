ed6e4e8954f5abe2011763011586481ff4db4322
hildebrandom <hildebrando.montoya@gmail.com>
2023-07-17 00:08:48 -0500

    Finish date Class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConceptsPOO;
+using System;
+
+Console.WriteLine("CONCEPTS POO");
+Console.WriteLine("============");
+
+try
+{
+	Console.WriteLine(new Date(2024, 2, 29));
+	Console.WriteLine(new Date(1974, 9, 23));
+	Console.WriteLine(new Date(1985, 11, 30));
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);	
+}
+
+
+
+
