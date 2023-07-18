cd619d7e6be69dd850707a7b1084d6557e5e7e61
hildebrandom <hildebrando.montoya@gmail.com>
2023-07-17 22:16:45 -0500

    Employee, SalaryEmployee & CommissionPercentaje finish

---
+++ ConceptsPOO/ConceptsPOO/Employee.cs
@@ -0,0 +1,28 @@
+﻿namespace ConceptsPOO
+{
+    public abstract class Employee
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Date BirthDate { get; set; }
+
+        public Date HiringDate { get; set; }
+
+        public bool IsActivate { get; set; }
+
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+            return $"{Id} - {FirstName} {LastName}, " +
+                $"Birth: {BirthDate}, " +
+                $"Hiring {HiringDate}, " +
+                $"Is activate :{IsActivate} ";
+        }
+
+    }
+}
--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -5,17 +5,31 @@ using System;
 
 Console.WriteLine("CONCEPTS POO");
 Console.WriteLine("============");
+Employee employee1 = new SalaryEmployee()
+{
+    Id = 1010,
+    FirstName = "Sandra",
+    LastName = "Morales",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 1, 15),
+    IsActivate = true,
+    Salary = 1815453.45M
+};
+Console.WriteLine(employee1);
 
-try
+Employee employee2 = new CommissionEmployee()
 {
-	Console.WriteLine(new Date(2024, 2, 29));
-	Console.WriteLine(new Date(1974, 9, 23));
-	Console.WriteLine(new Date(1985, 11, 30));
-}
-catch (Exception error)
-{
-	Console.WriteLine(error.Message);	
-}
+    Id = 2020,
+    FirstName = "Patricia",
+    LastName = "Gutierrez",
+    BirthDate = new Date(1995, 5, 23),
+    HiringDate = new Date(2022, 2, 5),
+    IsActivate = true,
+    Sales = 320000000M,
+    CommissionPercentaje = 0.03F
+    
+};
+Console.WriteLine(employee2);
 
 
 
